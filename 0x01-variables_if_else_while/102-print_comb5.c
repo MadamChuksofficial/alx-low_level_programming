548e67196c25df201c2a505afb54941232e92058
Chukwuebuka Agholor <officialchuksagholor@gmail.com>
2023-06-19 20:11:30 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main-entry point
+ * main - entry point;
  *
  * Description: prints all possible combinations of two two-digit numbers
  *
@@ -17,3 +17,15 @@ int main(void)
 		{
 			putchar('0' + i / 10);
 			putchar('0' + i % 10)
+			putchar(' ');
+			putchar('0' + j / 10);
+			putchar('0' + j % 10);
+
+			if (i == 98 && j == 99)
+				break;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
